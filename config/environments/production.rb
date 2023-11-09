ef221a41c3416e6577e9176ecb2669c1af5df734
sjdev2212 <daniel_amoroso31@hotmail.com>
2023-11-09 15:20:32 -0300

    host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.hosts << 'rails-production-ed19.up.railway.app'
+  config.hosts = [
+    'https://rails-production-ed19.up.railway.app/'
+  ]
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
