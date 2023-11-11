97d4dae00ddcb1b1453aaf47aff831008ede7e15
sjdev2212 <daniel_amoroso31@hotmail.com>
2023-11-10 22:21:02 -0300

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validates :email, presence: true, uniqueness: true
+    validates :name, presence: true
+    validates :password, presence: true, length: { minimum: 6 }, on: :create
+
     
 end
