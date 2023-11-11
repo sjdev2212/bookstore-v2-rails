9a7865222c005faed6c0bb5f344d864d7272a9da
sjdev2212 <daniel_amoroso31@hotmail.com>
2023-11-11 12:16:32 -0300

    auth session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
     def create
       user = User.find_by(email: params[:email])
       if user && user.authenticate(params[:password])
