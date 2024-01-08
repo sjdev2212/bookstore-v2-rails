f3b6710b406c712db87daa997eeeb5daa1bfdbd4
sjdev2212 <daniel_amoroso31@hotmail.com>
2024-01-08 15:41:19 -0300

    add role to params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :role)
   end
 end
