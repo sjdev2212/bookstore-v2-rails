d6137a1c2230b932595f304672d7640d1f13706a
sjdev2212 <daniel_amoroso31@hotmail.com>
2024-01-08 15:41:31 -0300

    add validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
   validates :password, presence: true, length: { minimum: 6 }, on: :create
+  validates :role, presence: { default: 'user' }
 end
