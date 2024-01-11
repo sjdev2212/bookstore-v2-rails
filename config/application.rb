c1cd85b5fb66d085be532abe9145202a4234fdef
sjdev2212 <daniel_amoroso31@hotmail.com>
2024-01-11 12:41:33 -0300

    remove cors from application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,12 +21,7 @@ module RubyRailsPostgres
       end
     end
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'http://localhost:3000'
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete], credentials: true
-      end
-    end
+
 
   end
 end
