9a523844120cf5b1de1acf00d01d04b9744ed60b
sjdev2212 <daniel_amoroso31@hotmail.com>
2024-01-18 12:03:54 -0300

    add methods to author controller

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,36 @@
+class AuthorsController < ApplicationController
+    def index 
+        @authors  = Author.all
+    end 
+
+    def show  
+
+        @author = Author.find_by_id(
+            params[:id]
+        )
+    end 
+
+    def new 
+        @author  = Author.new
+end
+
+def create 
+    @author = Author.new(author_params)
+
+    respond_to do |format|
+        if @author.save
+            format.html { redirect_to author_url(author), notice: "Author was successfully created." }
+            format.json { render :show, status: :created, location: author }
+        else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: author.errors, status: :unprocessable_entity }
+        end
+    end
+end 
+
+private
+
+def author_params 
+    params.require(:author).permit(:name, :date_of_birth, :biography, :nationality, :image)
+end 
+end
